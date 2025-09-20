0abe57d4341f39b0318fae606811f7d6c438295a
Asror <baxronovasror77@gmail.com>
2025-09-20 15:29:12 +0500

    Uyga vazifa

--- hello.py
+++ hello.py
@@ -1,88 +1,118 @@
+#######
+#1#
+#print("Hello world")
+# #2#
+# ism=str(input("isimni kiriting: "))
+# print("Hello!" ,ism,sep=" ")
+#3#
+# ism=str(input("isimni kiriting: "))
+# familya=str(input("familyani kiriting: "))
+# print(ism,familya,sep=" ")
+#4$
+# a=int(input("birinchi sonni kiriting: "))
+# b=int(input("ikkinchi sonni kiriting: "))
+# c=int(input("uchinchi sonni kiriting: "))
+# print(a,b,c,sep=", ")
+#5
+# print("Python","is",sep=" ", end="\n""fun")
+#6
+# age=str(input(" yoshingiz nechida: "))
+# print(age,"yoshda",sep=" ")
+#7
+#print("*"*10,"Welcome","*"*10,sep="")
+#8
+# a=int(input("birinchi sonni kiriting: "))
+# b=str(input("so'zni kiriting: "))
+# print(b*a)
+# #9
+# xabar=input("xabarni kiriting: ")
+# print(f"Bugungi yangiliklar:{xabar}")
+#10
+# print("A","B","C",sep="\n")
 ######### variables
-print("hello github!")
-# 1
-my_number = 10
-print(my_number)
+# # 1
+# my_number = 10
+# print(my_number)
 
-# 2
-my_decimal = 3.14
-print(type(my_decimal))
+# # 2
+# my_decimal = 3.14
+# print(type(my_decimal))
 
-# 3
-is_true = True
-print("Bu o‘zgaruvchi:", is_true)
+# # 3
+# is_true = True
+# print("Bu o‘zgaruvchi:", is_true)
 
-# 4
-name = "Asror"
-age = 20
-print("Mening ismim", name, "va yoshim", age)
+# # 4
+# name = "Asror"
+# age = 20
+# print("Mening ismim", name, "va yoshim", age)
 
-# 5. 
-x, y, z = 5, 10, 15
-print(x, y, z)
+# # 5. 
+# x, y, z = 5, 10, 15
+# print(x, y, z)
 
-# 6
-a = 5
-b = a
-print(b)
+# # 6
+# a = 5
+# b = a
+# print(b)
 
-# 7
-s = "123"
-s = int(s)
-print(type(s))
+# # 7
+# s = "123"
+# s = int(s)
+# print(type(s))
 
-# 8
-num_str = input("Son kiriting: ")
-num_int = int(num_str)
-print(num_int)
+# # 8
+# num_str = input("Son kiriting: ")
+# num_int = int(num_str)
+# print(num_int)
 
-# 9
-pi = 3.14159
-print(f"pi ning qiymati {pi}")
+# # 9
+# pi = 3.14159
+# print(f"pi ning qiymati {pi}")
 
-# 10
-a = 7
-b = 9
-a, b = b, a
-print("a =", a, "b =", b)
-################ arfimetika
-# 1
-num1 = 15
-num2 = 4
-print("Yig‘indi:", num1 + num2)
+# # 10
+# a = 7
+# b = 9
+# a, b = b, a
+# print("a =", a, "b =", b)
+# ################ arfimetika
+# # 1
+# num1 = 15
+# num2 = 4
+# print("Yig‘indi:", num1 + num2)
 
-# 2
-print("50 - 23 =", 50 - 23)
+# # 2
+# print("50 - 23 =", 50 - 23)
 
-# 3
-print("5 * 9 =", 5 * 9)
+# # 3
+# print("5 * 9 =", 5 * 9)
 
-# 4
-print("100 / 10 =", 100 / 10)
+# # 4
+# print("100 / 10 =", 100 / 10)
 
-# 5
-print("17 // 3 =", 17 // 3)
+# # 5
+# print("17 // 3 =", 17 // 3)
 
-# 6
-print("25 % 7 =", 25 % 7)
+# # 6
+# print("25 % 7 =", 25 % 7)
 
-# 7
-print("(10 + 5) * 2 =", (10 + 5) * 2)
+# # 7
+# print("(10 + 5) * 2 =", (10 + 5) * 2)
 
-# 8
-a = int(input("Birinchi sonni kiriting: "))
-b = int(input("Ikkinchi sonni kiriting: "))
-print("Yig‘indi:", a + b)
-print("Ayirma:", a - b)
-print("Ko‘paytma:", a * b)
-print("Bo‘linma:", a / b)
+# # 8
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# print("Yig‘indi:", a + b)
+# print("Ayirma:", a - b)
+# print("Ko‘paytma:", a * b)
+# print("Bo‘linma:", a / b)
 
-# 9
-num = int(input("Son kiriting: "))
-print("Kvadrat:", num ** 2)
+# # 9
+# num = int(input("Son kiriting: "))
+# print("Kvadrat:", num ** 2)
 
-# 10
-tshirt = 25
-hat = 15
-total = tshirt + hat
-print("Umumiy narx: $", total)
+# # 10
+# tshirt = 25
+# hat = 15
+# total = tshirt + hat
+# print("Umumiy narx: $", total)
